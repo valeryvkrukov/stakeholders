692389319d5c9f5374197691cdd40ecacad796d7
valeryvkrukov <valery.v.krukov@gmail.com>
2016-12-21 16:29:32 +0200

    initial

---
+++ src/Stakeholders/ApiBundle/StakeholdersApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Stakeholders\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StakeholdersApiBundle extends Bundle
+{
+}
---
+++ src/Stakeholders/ClientBundle/StakeholdersClientBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Stakeholders\ClientBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StakeholdersClientBundle extends Bundle
+{
+}
